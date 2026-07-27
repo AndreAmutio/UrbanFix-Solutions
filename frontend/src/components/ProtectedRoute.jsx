fdccf302fc52088b4b89e5c28d48bcff50297a2a
Andrea Amutio <andiamutio93@gmail.com>
2026-07-26 22:24:15 -0300

    Implementacion de login y configuración de API

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,15 +1,25 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-export default function ProtectedRoute({ children }) {
-  const { token, loading } = useAuth();
+export default function ProtectedRoute({ children, role }) { //children: panel agregao que se quiere proteger /role: rol requerido por la ruta
+  const { user, token, loading } = useAuth(); //obtengo el usuario
 
   if (loading) {
     return <p className="p-6 text-center">Cargando...</p>;
   }
 
-  if (!token) {
+  if (!token || !user) { //se comprueba que exista tanto el token como el usuario. Si falta alguno, vuelve al login.
     return <Navigate to="/login" replace />;
+  }
+
+  if (role && user.role !== role) {
+    const redirects = {
+      CLIENTE: "/cliente",
+      TECNICO: "/tecnico",
+      ADMIN: "/admin",
+    };
+
+    return <Navigate to={redirects[user.role] || "/"} replace />;
   }
 
   return children;
